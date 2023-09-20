f7b1a996996ffca4398df9356fe8711f3be4a8e4
sdpb21 <jytprf@gmail.com>
2023-09-20 18:20:31 -0300

    Testing try-exception to catch possible errors

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas
 import talib
 
 symbol = "BTCFDUSD"
-timeframe = "1m"
+timeframe = "15m"
 number_of_candles = 200
 rsi_size = 2
 ema_size = 7
@@ -35,36 +35,44 @@ pandas.set_option('display.max_columns', None)
 
 # candles_dataframe[ema_name] = talib.EMA(candles_dataframe['close'], timeperiod=ema_size)
 # print(candles_dataframe)
-
+e = "None"
 while True:
-    # Retrieve the candles / OHLC data
-    candles = spot_client.klines(
-        symbol=symbol,
-        interval=timeframe,
-        limit=number_of_candles
-    )
-    # Convert to a dataframe
-    candles_dataframe = pandas.DataFrame(candles)
-    # Step 4: Format the columns of the Dataframe.
-    # Documentation: https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md#klinecandlestick-data
-    candles_dataframe.columns = ["time", "open", "high", "low", "close", "volume", "close Time", "Quote Asset Volume",
-                                 "Number of Trades", "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume",
-                                 "Ignore"]
-    # Add a human time column which is based on a DateTime fo the 'time' column
-    candles_dataframe['human_time'] = pandas.to_datetime(candles_dataframe['time'], unit='ms')
-    # Make sure that the "open", "high", "low", "close", "volume" columns are floats
-    candles_dataframe[["open", "high", "low", "close", "volume"]] = candles_dataframe[
-        ["open", "high", "low", "close", "volume"]].astype(float)
 
-    rsi = talib.RSI(candles_dataframe['close'], timeperiod=rsi_size).iloc[-1]
-    print(rsi)
-    if rsi <= 1:
-        price = float(spot_client.ticker_price(symbol).get('price'))
-        print("buy price:", price)
-        while True:
-            playsound("/home/asdf/Downloads/beep-04.wav")
-            time.sleep(1.0)
-    time.sleep(1.0)
+    try:
+
+        # Retrieve the candles / OHLC data
+        candles = spot_client.klines(
+            symbol=symbol,
+            interval=timeframe,
+            limit=number_of_candles
+        )
+        # Convert to a dataframe
+        candles_dataframe = pandas.DataFrame(candles)
+        # Step 4: Format the columns of the Dataframe.
+        # Documentation: https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md#klinecandlestick-data
+        candles_dataframe.columns = ["time", "open", "high", "low", "close", "volume", "close Time", "Quote Asset Volume",
+                                     "Number of Trades", "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume",
+                                     "Ignore"]
+        # Add a human time column which is based on a DateTime fo the 'time' column
+        candles_dataframe['human_time'] = pandas.to_datetime(candles_dataframe['time'], unit='ms')
+        # Make sure that the "open", "high", "low", "close", "volume" columns are floats
+        candles_dataframe[["open", "high", "low", "close", "volume"]] = candles_dataframe[
+            ["open", "high", "low", "close", "volume"]].astype(float)
+
+        rsi = talib.RSI(candles_dataframe['close'], timeperiod=rsi_size).iloc[-1]
+        print(rsi, "Exception: ", e)
+        if rsi <= 1:
+            price = float(spot_client.ticker_price(symbol).get('price'))
+            print("buy price:", price)
+            while True:
+                playsound("/home/asdf/Downloads/beep-04.wav")
+                time.sleep(1.0)
+        time.sleep(1.0)
+
+    except Exception as e:
+
+        print("Exception: ", e)
+        continue
 
     # ema = int(talib.EMA(candles_dataframe['close'], timeperiod=ema_size).iloc[-1])
     # ema2 = int(talib.EMA(candles_dataframe['close'], timeperiod=ema_size2).iloc[-1])
