f53c028101b91d60abb72077ec347b8349ab8388
sdpb21 <jytprf@gmail.com>
2024-01-07 19:07:26 -0300

    Changes added to main3.py to display the total profit

--- main3.py
+++ main3.py
@@ -6,11 +6,13 @@ import datetime
 import time
 
 symbol = "BTCFDUSD"
-usd = 200
+usd = 100
 number_of_candles = 200
-rsi_size = 4
+rsi_size = 2
 buy = False
 buyPrice = 100000
+quantity = 0
+sumProfit = 0
 
 if __name__ == '__main__':
     spot_client = Client(api_key=config.APY_KEY, api_secret=config.APY_SECRET_KEY, tld='com')
@@ -19,8 +21,8 @@ if __name__ == '__main__':
         candles = spot_client.get_historical_klines(
             symbol=symbol,
             # interval=Client.KLINE_INTERVAL_1HOUR,
-            # interval=Client.KLINE_INTERVAL_15MINUTE,
-            interval=Client.KLINE_INTERVAL_1MINUTE,
+            interval=Client.KLINE_INTERVAL_15MINUTE,
+            # interval=Client.KLINE_INTERVAL_1MINUTE,
             limit=number_of_candles
         )
 
@@ -41,10 +43,11 @@ if __name__ == '__main__':
 
         # time.sleep(0.3)
 
-        if not buy and rsi < 20.0:
+        if not buy and rsi < 25.0:
             buy = True
             buyPrice = float(spot_client.get_symbol_ticker(symbol=symbol).get('price'))
             print("************************************ buy price:", buyPrice)
+            quantity = usd / buyPrice
             # comprar
 
         actualPrice = float(spot_client.get_symbol_ticker(symbol=symbol).get('price'))
@@ -55,11 +58,15 @@ if __name__ == '__main__':
         if buy and buyPrice < actualPrice and boolMinutes:
             # vender
             print("************************************ sell price:", actualPrice)
-            if rsi <= 80.0:
+            usdAfterSell = quantity * actualPrice
+            profit = usdAfterSell - usd
+            sumProfit += profit
+            print("profit:", profit, "sumProfit: ", sumProfit)
+            if rsi <= 75.0:
                 buyPrice = float(spot_client.get_symbol_ticker(symbol=symbol).get('price'))
                 print("************************************ buy price:", buyPrice)
                 # comprar otra vez
-            if rsi > 80.0:
+            if rsi > 75.0:
                 buy = False
 
-        time.sleep(60.0)
+        time.sleep(59.0)
