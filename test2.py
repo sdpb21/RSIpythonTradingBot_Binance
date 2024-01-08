b99d962a552d96e27b4e47e911d47cdb4c86e63b
sdpb21 <jytprf@gmail.com>
2024-01-08 14:02:45 -0300

    Printing EMA, looking for a new strategy

---
+++ test2.py
@@ -0,0 +1,36 @@
+from binance.client import Client
+import config
+import pandas
+import talib
+
+symbol = "BTCFDUSD"
+number_of_candles = 200
+ema_size = 7
+
+if __name__ == '__main__':
+
+    spot_client = Client(api_key=config.APY_KEY, api_secret=config.APY_SECRET_KEY, tld='com')
+    while True:
+
+        candles = spot_client.get_historical_klines(
+            symbol=symbol,
+            # interval=Client.KLINE_INTERVAL_1HOUR,
+            interval=Client.KLINE_INTERVAL_15MINUTE,
+            # interval=Client.KLINE_INTERVAL_1MINUTE,
+            limit=number_of_candles
+        )
+
+        candles_dataframe = pandas.DataFrame(candles)
+
+        candles_dataframe.columns = ["time", "open", "high", "low", "close", "volume", "close Time",
+                                     "Quote Asset Volume", "Number of Trades", "Taker Buy Base Asset Volume",
+                                     "Taker Buy Quote Asset Volume", "Ignore"]
+
+        candles_dataframe['human_time'] = pandas.to_datetime(candles_dataframe['time'], unit='ms')
+
+        candles_dataframe[["open", "high", "low", "close", "volume"]] = candles_dataframe[
+            ["open", "high", "low", "close", "volume"]].astype(float)
+
+        ema = int(talib.EMA(candles_dataframe['close'], timeperiod=ema_size).iloc[-1])
+
+        print(ema)
